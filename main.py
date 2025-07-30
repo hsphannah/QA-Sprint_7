2eb1af1bf66750ecb28d692748685ac19b24ea50
Hannah <preigschadth@gmail.com>
2025-07-30 18:23:51 -0300

    atualizações

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-python --version
+import data
+import helpers
+
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes ")
+        else:
+            print("Não foi possivel conectar ao Urban Routes.Verifique se o servidor está ligado e ainda em execução")
+
+    def test_set_route(self):
+        #Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        #Adicionar em S8
+        print("função criada para a seleção de plano")
+        pass
+
+    def test_fill_phone_number(self):
+        #Adicionar em S8
+        print("função criada para preencher o numero de telefone ")
+        pass
+
+    def test_fill_card(self):
+        #Adicionar em S8
+        print("função criada para preencher o cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        #Adicionar em S8
+        print("função criada para comentar a corrida")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #Adicionar em S8
+        print("função criada para pedir cobertor e lenço")
+        pass
+
+    def test_order_2_ice_creams(self):
+        number_of_ice_creams =  2
+        for count in range(number_of_ice_creams): # Adicionado ':' e identado o bloco abaixo
+            #Adicionar em S8
+            print("função criada para pedir 2 sorvetes")
+            pass
+
+    def test_car_search_model_appears(self):
+        #Adicionar em S8
+        print("função criada para encontrar modelos dos carros")
+        pass
